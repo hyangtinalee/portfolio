1dfcb2316dc2bf75878419b9d09a188e17879223
Tina Lee <tlhyang@gmail.com>
2025-01-02 23:18:30 -0800

    feat: react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { About } from "./pages/About.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/portfolio/",
+        element: <App />,
+        children: [
+            {
+                path: "/portfolio/",
+                element: <Home />,
+            },
+            {
+                path: "/portfolio/about",
+                element: <About />,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
